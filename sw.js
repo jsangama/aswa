6ab81961cd0ffd4a8f63e8c239f92410deca5f51
sangama-tecsup <josue.sangama@tecsup.edu.pe>
2026-05-27 01:11:26 -0500

    Add persistent app install menu

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// ASWA Service Worker v3 - https://jsangama.github.io/aswa/
-const CACHE_NAME = 'aswa-v3';
+// ASWA Service Worker v4 - https://jsangama.github.io/aswa/
+const CACHE_NAME = 'aswa-v4';
 const BASE = '/aswa/';
 const ASSETS = [
   BASE,
