d77c55da77f9c05dd7747ef999ba385386883c4b
sangama-tecsup <josue.sangama@tecsup.edu.pe>
2026-05-31 23:17:44 -0500

    Show support chat messages immediately

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// ASWA Service Worker v23 - https://jsangama.github.io/aswa/
-const CACHE_NAME = 'aswa-v23';
+// ASWA Service Worker v24 - https://jsangama.github.io/aswa/
+const CACHE_NAME = 'aswa-v24';
 const BASE = new URL('./', self.registration.scope).pathname;
 const ASSETS = [
   BASE,
